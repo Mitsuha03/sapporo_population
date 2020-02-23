63455943278d5e610c6d7876be038656a89477b1
mitsuha03 <miyake@mierune.co.jp>
2020-02-23 15:09:12 +0900

    人口の値によって町丁目を色分け

--- js/d3.js
+++ js/d3.js
@@ -14,22 +14,28 @@ var path = d3.geoPath()
                     .translate([width / 2, height / 2])
             );
 
+var t = d3.scaleLinear()
+            .domain([0, 1000])
+            .range([1, 0]);
+
 d3.json("/ut_open/data/h27ka01101.json").then(function (data){
-    // if (err) throw err
-    // console.log(data[0]);
     svg.selectAll("path")
         .data(data.features)
         .enter().append("path")
         .attr("d", path)
+        // 人口の値によって色分け
+        .style("fill", function(d) {return d3.interpolateRdYlGn(t(d.properties.JINKO));})
+        // マウスホバーでピンク色
+        .on("mouseover", function(d){
+            d3.select(this).style("fill", "#ff1493")
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .style("fill", function(d) {return d3.interpolateRdYlGn(t(d.properties.JINKO));})
+            })
+        // クリックで町丁目の人口をアラート表示
         .on("click", function(d){
             alert(d.properties.MOJI+"の人口は"+d.properties.JINKO+"です。"
             )
         })
-        .on("mouseover", function(d){
-            d3.select(this).style("fill", "#ef0211")
         })
-        .on("mouseout", function(d) {
-            d3.select(this)
-                .style("fill", "#ffffff")
-        });
-    })
