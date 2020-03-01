05046bbeb190d0767c2531596174f590efbdc6ec
mitsuha03 <miyake@mierune.co.jp>
2020-03-01 17:41:42 +0900

    棒グラフに町丁目ラベルを追加

--- js/d3.js
+++ js/d3.js
@@ -1,16 +1,18 @@
 // 地図部分
-var width = 800;
-var height = 600;
+const mapWidth = 800;
+const mapHeight = 600;
+const graphWidth = 1000;
+const graphHeight = 15000;
 
 var map = d3.select("#mapChuo")
-                .attr("width", width)
-                .attr("height", height);
+                .attr("width", mapWidth)
+                .attr("height", mapHeight);
 
 var path = d3.geoPath()
                 .projection(d3.geoMercator()
                     .center([141.30, 43.03])
                     .scale(200000)
-                    .translate([width / 2, height / 2])
+                    .translate([mapWidth / 2, mapHeight / 2])
                 );
 
 var t = d3.scaleLinear()
@@ -25,8 +27,8 @@ var zoom = d3.zoom()
 map.call(zoom)
 
 var chart = d3.select("#graphChuo")
-                .attr("width", 1000)
-                .attr("height", 600);
+                .attr("width", graphWidth)
+                .attr("height", graphHeight);
 
 d3.json("./data/h27ka01101.json").then(function (data){
     map.selectAll("path")
@@ -50,13 +52,36 @@ d3.json("./data/h27ka01101.json").then(function (data){
 
     // 棒グラフ部分
     chart.selectAll("rect")
-            .data(data.features)
-            .enter()
-            .append("rect")
-            .attr("x", function(d) {return ;})
-            .attr("y", function(d, i) {return 10 * i})
-            .attr("width", function(d) {return d.properties.JINKO})
-            .attr("height", "9")
+    .data(data.features)
+    .enter()
+    .append("rect")
+    .attr("x", function(d) {return 150;})
+    .attr("y", function(d, i) {return i * (graphHeight / data.features.length)})
+    .attr("width", function(d) {return d.properties.JINKO})
+    .attr("height", graphHeight / data.features.length)
+    // 人口の値によって色分け
+    .style("fill", function(d) {return d3.interpolateRdYlGn(t(d.properties.JINKO));})
+    // マウスホバーでピンク色
+    .on("mouseover", function(){
+        d3.select(this).style("fill", "#ff1493")
+    })
+    .on("mouseout", function(d) {
+        d3.select(this)
+        .style("fill", function(d) {return d3.interpolateRdYlGn(t(d.properties.JINKO));})
+    })
+    // クリックで町丁目の人口をアラート表示
+    .on("click", function(d){
+        alert(d.properties.MOJI+"の人口は"+d.properties.JINKO+"です。")
+    });
+
+    chart.selectAll("text")
+    .data(data.features)
+    .enter()
+    .append("text")
+    .text(function(d) {return d.properties.MOJI})
+    .attr("x", function(d) {return 0;})
+    .attr("dominant-baseline", "central")
+    .attr("y", function(d, i) {return i * (graphHeight / data.features.length) + (graphHeight / data.features.length - 2) / 2})
 });
 
 // ズーム機能
